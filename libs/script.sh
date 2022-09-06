1ee1c3627812e4eef8b35ed3deb9e1f0e0c8c4da
Xalalau <xalalau@outlook.com>
2022-09-06 02:44:47 -0300

    Improve scripts logging

--- libs/script.sh
+++ libs/script.sh
@@ -1,6 +1,6 @@
 function runScript() {
     # $1 = script filename
-    
-    printfHr "Running '$1.sh'"
+    printfHr "Starting '$1.sh'"
     source "$DIR_SCRIPTS/$1.sh"
+    printfHr "Finishing '$1.sh'"
 }
