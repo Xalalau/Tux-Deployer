de12b8d24158d31f9f94764add2468489a7b7331
Xalalau <xalalau@outlook.com>
2022-09-08 13:07:35 -0300

    Improve script finished message

--- libs/script.sh
+++ libs/script.sh
@@ -2,5 +2,5 @@ function runScript() {
     # $1 = script filename
     printfHr "Starting '$1.sh'"
     source "$DIR_SCRIPTS/$1.sh"
-    printfHr "Finishing '$1.sh'"
+    printfHr "Finished '$1.sh'"
 }
