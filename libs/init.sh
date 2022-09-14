fed86234edeb886afa1110213fa5cee4f2b3207b
Xalalau <xalalau@outlook.com>
2022-09-14 01:55:41 -0300

    Fix init order

--- libs/init.sh
+++ libs/init.sh
@@ -13,8 +13,6 @@ else
     source "/etc/lsb-release"
 fi
 
-source "$DIR_BASE/config.sh"
-
 NOW="$(date)"
 NOW_FORMATED="$(echo $NOW | tr -s '[:blank:]' '_')"
 
@@ -35,88 +33,88 @@ COLOR_FAILED="\e[1;31m" # Red
 COLOR_CRITICAL="\e[1;31m" # Red
 COLOR_HR="\e[1;32m" # Green
 
-{ # Init
-    cd "$DIR_LIBS"
-    for file in *; do
-        if [ $file != "init.sh" ]; then
-            source "$file"
-        fi
-    done
+ARCH="$(dpkg --print-architecture)"
 
-    if [ $DISTRIB_ID != "Ubuntu" ]; then
-        printfCritical "Tux Deployer only supports Ubuntu."
-        exit
+USER="$(whoami)"
+
+FILE_NETPLAN="$(cd "$DIR_NETWORK"; for dir in *; do echo "$DIR_NETWORK/$dir"; done;)"
+
+NETWORK_INTERFACE="$(ip route | awk '/default/ {print $5; exit}')"
+NETWORK_RENDERER="$(cat "$FILE_NETPLAN" | awk '/renderer/ {print $2; exit}')"
+GATEWAY="$(ip route | awk '/default/ {print $3; exit}')"
+IP_INTERNAL="$(hostname -I | cut -d' ' -f1)"
+SUBNET=$(ip route | awk '/proto/ && !/default/ {print $1}' | cut -d '/' -f2)
+
+read MAC </sys/class/net/$NETWORK_INTERFACE/address
+MAC_FORMATTED_LOWERCASE="$(echo $MAC | tr -d :)"
+MAC_FORMATTED_UPPERCASE="${MAC_FORMATTED_LOWERCASE^^}"
+
+if [ ${DISTRIB_RELEASE:0:2} -ge 22 ]; then
+    IS_APT_KEY_DEPRECATED=1
+else
+    export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 # Hide a warning since Ubuntu 18.04
+    IS_APT_KEY_DEPRECATED=0
+fi
+
+source "$DIR_BASE/config.sh"
+
+cd "$DIR_LIBS"
+for file in *; do
+    if [ $file != "init.sh" ]; then
+        source "$file"
     fi
+done
 
-    if [ "$DISTRIB_CODENAME" != "focal" ] && [ "$DISTRIB_CODENAME" != "jammy" ]; then
-        printfWarning "Warning! This script was tested on Ubuntu 20.04 (focal) and Ubuntu 22.04 (jammy), so tweaks may be needed in your version ($DISTRIB_CODENAME)."
-    fi
+if [ $DISTRIB_ID != "Ubuntu" ]; then
+    printfCritical "Tux Deployer only supports Ubuntu."
+    exit
+fi
 
-    mkdir -p "$DIR_LOGS"
-    echo "" > "$FILE_LOG"
+if [ "$DISTRIB_CODENAME" != "focal" ] && [ "$DISTRIB_CODENAME" != "jammy" ]; then
+    printfWarning "Warning! This script was tested on Ubuntu 20.04 (focal) and Ubuntu 22.04 (jammy), so tweaks may be needed in your version ($DISTRIB_CODENAME)."
+fi
 
-    cd "$DIR_BASE"
+mkdir -p "$DIR_LOGS"
+echo "" > "$FILE_LOG"
 
-    ARCH="$(dpkg --print-architecture)"
+cd "$DIR_BASE"
 
-    USER="$(whoami)"
+printfWarning "To stop the installation at any time, press CTRL+C."
+printfWarning "Press any key to start."
+read -r TEMP
 
-    FILE_NETPLAN="$(cd "$DIR_NETWORK"; for dir in *; do echo "$DIR_NETWORK/$dir"; done;)"
+sudo clear
 
-	NETWORK_INTERFACE="$(ip route | awk '/default/ {print $5; exit}')"
-	NETWORK_RENDERER="$(cat "$FILE_NETPLAN" | awk '/renderer/ {print $2; exit}')"
-    GATEWAY="$(ip route | awk '/default/ {print $3; exit}')"
-	IP_INTERNAL="$(hostname -I | cut -d' ' -f1)"
-	SUBNET=$(ip route | awk '/proto/ && !/default/ {print $1}' | cut -d '/' -f2)
+printfHr "$SCRIPT_NAME"
+printfHr """$SCRIPT_LICENSE"""
+printfInfo "    Log: $NOW_FORMATED.txt"
+echo
 
-	read MAC </sys/class/net/$NETWORK_INTERFACE/address
-	MAC_FORMATTED_LOWERCASE="$(echo $MAC | tr -d :)"
-	MAC_FORMATTED_UPPERCASE="${MAC_FORMATTED_LOWERCASE^^}"
+commandExists "unzip"
+if [ "$?" -ne 1 ]; then
+    installApt "unzip"
+fi
 
-    if [ ${DISTRIB_RELEASE:0:2} -ge 22 ]; then
-        IS_APT_KEY_DEPRECATED=1
-    else
-        export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 # Hide a warning since Ubuntu 18.04
-        IS_APT_KEY_DEPRECATED=0
-    fi
+commandExists "unrar"
+if [ "$?" -ne 1 ]; then
+    installApt "unrar"
+fi
 
-	printfWarning "To stop the installation at any time, press CTRL+C."
-	printfWarning "Press any key to start."
-    read -r TEMP
-
-    sudo clear
-
-    printfHr "$SCRIPT_NAME"
-    printfHr """$SCRIPT_LICENSE"""
-    printfInfo "    Log: $NOW_FORMATED.txt"
-    echo
-
-    commandExists "unzip"
+if [ $ENABLE_FLATPAK -eq 1 ]; then
+    commandExists "flatpak"
     if [ "$?" -ne 1 ]; then
-        installApt "unzip"
+        installApt "flatpak"
     fi
+fi
 
-    commandExists "unrar"
+if [ $ENABLE_SNAP -eq 1 ]; then
+    commandExists "snap"
+
     if [ "$?" -ne 1 ]; then
-        installApt "unrar"
-    fi
-
-    if [ $ENABLE_FLATPAK -eq 1 ]; then
-        commandExists "flatpak"
-        if [ "$?" -ne 1 ]; then
-            installApt "flatpak"
+        if [ -f "/etc/apt/preferences.d/nosnap.pref" ]; then # Linux Mint
+            sudo rm "/etc/apt/preferences.d/nosnap.pref"
+            sudo apt update &>>"$FILE_LOG";
         fi
+        installApt "snapd"
     fi
-
-    if [ $ENABLE_SNAP -eq 1 ]; then
-        commandExists "snap"
-
-        if [ "$?" -ne 1 ]; then
-            if [ -f "/etc/apt/preferences.d/nosnap.pref" ]; then # Linux Mint
-                sudo rm "/etc/apt/preferences.d/nosnap.pref"
-                sudo apt update &>>"$FILE_LOG";
-            fi
-            installApt "snapd"
-        fi
-    fi
-}
+fi
