6b10a0c84973abc6e6abeb4f1b25cb5bd4f32b5f
Xalalau <xalalau@outlook.com>
2022-09-14 15:19:22 -0300

    Improve file logs

--- libs/log.sh
+++ libs/log.sh
@@ -2,22 +2,26 @@ function printfDebug() {
     # $1 = Message
     if [ $DEBUG -eq 1 ]; then
         printf "$COLOR_BACKGROUND$COLOR_DEBUG%s\e[m\e[m\n" "$1"
+        echo $1 &>>"$FILE_LOG";
     fi
 }
 
 function printfInfo() {
     # $1 = Message
     printf "$COLOR_BACKGROUND$COLOR_INFO%s\e[m\e[m\n" "$1"
+    echo $1 &>>"$FILE_LOG";
 }
 
 function printfWarning() {
     # $1 = Message
     printf "$COLOR_BACKGROUND$COLOR_WARNING%s\e[m\e[m\n" "$1"
+    echo $1 &>>"$FILE_LOG";
 }
 
 function printfError() {
     # $1 = Message
     printf "$COLOR_BACKGROUND$COLOR_FAILED%s\e[m\e[m\n" "$1"
+    echo $1 &>>"$FILE_LOG";
 
     if [ $HALT_ON_FAILED -eq 1 ]; then
         exit
@@ -27,6 +31,7 @@ function printfError() {
 function printfCritical() {
     # $1 = Message
     printf "\e[4m$COLOR_BACKGROUND$COLOR_CRITICAL%s\e[m\e[m\e[0m\n" "$1"
+    echo $1 &>>"$FILE_LOG";
 
     if [ $HALT_ON_CRITICAL -eq 1 ]; then
         exit
@@ -36,4 +41,5 @@ function printfCritical() {
 function printfHr() {
     # $1 = Message
     printf "$COLOR_BACKGROUND$COLOR_HR%s\e[m\e[m\n" "$1"
+    echo $1 &>>"$FILE_LOG";
 }
