e8bbec43c89b324b57ba1b610da4f76535f296ee
Xalalau <xalalau@outlook.com>
2022-09-14 11:52:32 -0300

    Add more comments to config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,32 @@
+# Usefull global variables
+<<comment
+    $DISTRIB_ID               Distribution name
+    $DISTRIB_RELEASE          Distribution code
+    $DISTRIB_CODENAME         Distribution codename
+    $DISTRIB_DESCRIPTION      Full distribution name
+    $NOW                      Current time
+    $ARCH                     System architecture
+    $USER_CURRENT             Current logged user
+	$NETWORK_INTERFACE        Main network interface name
+	$NETWORK_RENDERER         Network daemon
+    $GATEWAY                  Network gateway
+	$IP_INTERNAL              Current internal IP
+	$SUBNET                   Network mask
+	$MAC                      Network MAC
+	$MAC_FORMATTED_LOWERCASE  Network MAC without : and lowercase
+	$MAC_FORMATTED_UPPERCASE  Network MAC without : and uppercase
+comment
+
 # -------------------------------
-# Script settings
+# Installer settings
 # -------------------------------
 
 HALT_ON_FAILED=0
 HALT_ON_CRITICAL=1
+
 ENABLE_FLATPAK=1
 ENABLE_SNAP=1
+
 DEBUG=1
 
 # -------------------------------
@@ -13,5 +34,5 @@ DEBUG=1
 # -------------------------------
 
 
-# Add global variables here
+# Add your global variables/settings here
 
