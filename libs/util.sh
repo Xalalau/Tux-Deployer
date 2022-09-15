50a1f312b0886eab44717e5765e909f199291c54
Xalalau <xalalau@outlook.com>
2022-09-15 14:45:27 -0300

    Add git.sh

--- libs/util.sh
+++ libs/util.sh
@@ -176,27 +176,3 @@ function download() {
         return 1
     fi
 }
-
-function gitClone() {
-	# $1 = Repository
-    # Returns: 1 [Success] / 0 [Failed]
-	local link=$1
-	local dir=$(basename $link .git)
-
-	run "cd $dir"
-	if [ "$?" -eq 1 ]; then
-		printfDebug "Already cloned $dir"
-		return 1
-	else
-		git clone $link &>>"$FILE_LOG";
-	fi
-	run "cd $dir"
-	if [ "$?" -eq 1 ]; then
-		printfDebug "Finished cloning $dir"
-	else
-		printfError "Failed to clone $dir"
-		return 0
-	fi
-
-	return 1
-}
