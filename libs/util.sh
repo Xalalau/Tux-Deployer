55c8db24da9cfed5fbe64ced12d8aaaf9228d93e
Xalalau <xalalau@outlook.com>
2022-11-24 10:46:50 -0300

    Remove more tabs

--- libs/util.sh
+++ libs/util.sh
@@ -1,34 +1,34 @@
 function run() {
-	# $1 = Bash command
+    # $1 = Bash command
     # Returns: 1 [No errors] / 0 [Errors occurred]
-	local error=$((($1) 1>>"$FILE_LOG";) 2>&1)
+    local error=$((($1) 1>>"$FILE_LOG";) 2>&1)
 
     if [ "$error" != "" ]; then
         echo $error &>>"$FILE_LOG";
-		return 0
-	else
-		return 1
-	fi
+        return 0
+    else
+        return 1
+    fi
 }
 
 function createDir() {
-	# $1 = Directory
-	if [ -d "$1" ]; then
-		printfDebug "Already created \"$1\""
-	else
-		mkdir -p "$1"
-		printfDebug "\"$1\" created"
-	fi
+    # $1 = Directory
+    if [ -d "$1" ]; then
+        printfDebug "Already created \"$1\""
+    else
+        mkdir -p "$1"
+        printfDebug "\"$1\" created"
+    fi
 }
 
 function createDirSudo() {
-	# $1 = Directory
-	if [ -d "$1" ]; then
-		printfDebug "Already created \"$1\""
-	else
-		sudo mkdir -p "$1"
-		printfDebug "\"$1\" created"
-	fi
+    # $1 = Directory
+    if [ -d "$1" ]; then
+        printfDebug "Already created \"$1\""
+    else
+        sudo mkdir -p "$1"
+        printfDebug "\"$1\" created"
+    fi
 }
 
 function commandExists() {
