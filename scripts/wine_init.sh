ac8c1ea5950cc0eefdd48cb886276aeb669969bd
Xalalau Xubilozo <xalalau@outlook.com>
2023-06-11 04:10:41 -0300

    Prefer USER_CURRENT instead of USER

--- scripts/wine_init.sh
+++ scripts/wine_init.sh
@@ -1,9 +1,9 @@
 {
 	commandExists "wine"
 	if [ "$?" -eq 1 ]; then
-		if [ ! -d "/home/$USER/.wine/drive_c" ]; then
+		if [ ! -d "/home/$USER_CURRENT/.wine/drive_c" ]; then
 			printfInfo "Initializing Wine default perfix"
-			WINEPREFIX="/home/$USER/.wine" WINEARCH='win32' wine 'wineboot' &>>"$FILE_LOG";
+			WINEPREFIX="/home/$USER_CURRENT/.wine" WINEARCH='win32' wine 'wineboot' &>>"$FILE_LOG";
 		else
 			printfDebug "Wine default prefix already exists"
 		fi
