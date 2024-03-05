8f761bf18e3e54b06a966f409fb170211abb2659
Xalalau <xalalau@outlook.com>
2024-03-05 16:12:06 -0300

    Updates on gitClone to set a target dir

--- libs/git.sh
+++ libs/git.sh
@@ -1,21 +1,27 @@
 function gitClone() {
     # $1 = Repository
+    # $2 = folder
     # Returns: 1 [Success] / 0 [Failed]
     local link=$1
-    local dir=$(basename $link .git)
+    local dir="$2"
+    local name="$(basename $link .git)"
+    
+    if [ "$dir" = "" ]; then
+        dir="$name"
+    fi
 
     run "cd $dir"
     if [ "$?" -eq 1 ]; then
-        printfDebug "Already cloned $dir"
+        printfDebug "Already cloned $name"
         return 1
     else
-        git clone $link &>>"$FILE_LOG";
+        git clone $link "$dir" &>>"$FILE_LOG";
     fi
     run "cd $dir"
     if [ "$?" -eq 1 ]; then
-        printfDebug "Finished cloning $dir"
+        printfDebug "Finished cloning $name"
     else
-        printfError "Failed to clone $dir"
+        printfError "Failed to clone $name"
         return 0
     fi
 
