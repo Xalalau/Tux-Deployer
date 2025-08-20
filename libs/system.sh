38826d299a8671ddc4829a8bd3e9108b08b0f4e8
Xalalau Xubilozo <xalalau@outlook.com>
2025-08-20 14:12:56 -0300

    Add func addCronJob

--- libs/system.sh
+++ libs/system.sh
@@ -41,3 +41,41 @@ function commandExists() {
         return 0
     }
 }
+
+function addCronJob() {
+    # $1 = New cronjob
+    # $2 = Username (optional - default root)
+    # Returns: 1 [Failed] / 0 [Success]
+    local new_line="$1"
+    local user="$2"
+
+    if [[ -z "$user" ]]; then
+        user="root"
+    fi
+
+    if ! id -u "$user" >/dev/null 2>&1; then
+        printfError "User '$user' does not exist."
+        return 1
+    fi
+
+    if [[ -z "$new_line" ]]; then
+        printfError "Cron job not provided."
+        return 1
+    fi
+
+    local cron="$(sudo crontab -u "$user" -l 2>/dev/null || true)"
+
+    local updated_cron="$cron"
+    if [[ "$cron" != *"$new_line"* ]]; then
+        updated_cron+=$'\n'"$new_line"
+    fi
+
+    if [[ "$updated_cron" != "$cron" ]]; then
+        echo "$updated_cron" | sudo crontab -u "$user" -
+        printfDebug "Cron jobs updated for user \"$user\""
+    else
+        printfDebug "No new cron jobs to add for user \"$user\""
+    fi
+
+    return 0
+}
